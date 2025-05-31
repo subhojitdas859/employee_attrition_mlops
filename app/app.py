a98f1ee6f13cfa0ac794aa427a581f5060c2104b
Subhojit Das <38422567+subhojitdas859@users.noreply.github.com>
2025-05-31 15:44:55 +0000

    created kubernetes yaml files

--- app/app.py
+++ app/app.py
@@ -20,4 +20,4 @@ def predict():
     return render_template("index.html", prediction_text=f"Prediction: {result}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
